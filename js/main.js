339cd134d31ca16fa3551e64fba83c33fc3b2da6
Tiffany Liem <thuytliem@gmail.com>
2023-04-12 05:24:53 +0000

    Created DOMContentLoaded Event Listener and append each entry to the unordered list

--- js/main.js
+++ js/main.js
@@ -20,4 +20,32 @@ $form.addEventListener('submit', function (event) {
   $form.reset();
 });
 
-// function renderEntry (entry);
+function renderEntry(entry) {
+  const li = document.createElement('li');
+  li.setAttribute('class', 'column-half');
+  const firstDiv = document.createElement('div');
+  firstDiv.setAttribute('class', 'column-half');
+  li.appendChild(firstDiv);
+  const img = document.createElement('img');
+  img.setAttribute('class', 'image');
+  img.setAttribute('src', entry.photoURL);
+  firstDiv.appendChild(img);
+  const secondDiv = document.createElement('div');
+  secondDiv.setAttribute('class', 'column-half');
+  li.appendChild(secondDiv);
+  const h3 = document.createElement('h3');
+  h3.createTextNode(entry.title);
+  secondDiv.appendChild(h3);
+  const p = document.createElement('p');
+  p.createTextNode(entry.notes);
+  secondDiv.appendChild(p);
+  return li;
+}
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  const $ul = document.querySelector('ul');
+  for (let i = 0; i < data.entries.length; i++) {
+    $ul.appendChild(renderEntry(data.entries[i]));
+  }
+}
+);
